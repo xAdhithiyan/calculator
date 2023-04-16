4372d380700e07a07bfa5fa7c243b05eba2bbae9
adhithiyan <adhithiyan.work@gmail.com>
2023-04-16 22:14:44 +0530

    working model for multiple operation

--- script.js
+++ script.js
@@ -2,12 +2,19 @@ function operation(x,y,op){
     switch(op){
         case "+":
             return x + y;
+            break;
         case "-":
             return x - y;
+            break;
         case "x":
             return x * y;
+            break;
         case "/":
             return x / y;
+            break;
+        case "^":
+            return Math.pow(x,y)
+            break;
     }
 }
 
@@ -15,36 +22,53 @@ function operation(x,y,op){
 function operate(x = "",y = "",op = ""){
     const btn = document.querySelectorAll("button")
     let num = "";
-    //to decide wheater top or bottom ans
+    //to decide wheater top or bottom dispaly
     let TopOrBottom;
+    //to check for multiple operations
+    let noOfOp = 0;
+    let x1=0;
+
     btn.forEach((n) => n.addEventListener("click",function(){
+        // to remove last element entered
         if(this.textContent == "="){ 
             y = num;
             
             //push the operation into an array
-            allOperationArr.push(new OneOperation(x,y,op));
+            allOperationArr.push(new OneOperation(x1,y,op));
             //to display the final ans
-            display(x,y,op,1)
+            display(x,y,op,1,x1)
+
             TopOrBottom = 0;
+            noOfOp = 0;
             //to continue the operation
             num = allOperationArr[allOperationArr.length - 1].ans;
-            x = y = op = "";
-        }else if(!Number.isInteger(+this.textContent)){  //checking if user clicked an operator
-            x = num;
+             x = y = op = "";
+        }else if(!Number.isInteger(+this.textContent) && noOfOp > 0 && this.textContent != "."){
             op = this.textContent;
+            x = document.querySelector(".topAns").textContent;
             num = "";
             TopOrBottom = 1;
+            x1 = document.querySelector(".bottomAns").textContent;
+        }
+        else if(!Number.isInteger(+this.textContent) && this.textContent != "."){  //checking if user clicked an operator
+            op = this.textContent;
+            x = num;
+            num = "";
+            TopOrBottom = 1;
+            noOfOp ++;
+            x1=x;
+
         }
         else{
             num += this.textContent;
             TopOrBottom = 1;
         }
-
+        
         //to display ans in display area
         if(!op && TopOrBottom){
-            display(num,y,op)
+            display(num,y,op,0,x1)
         }else if(op && TopOrBottom){
-            display(x,num,op)
+            display(x,num,op,0,x1)
         }
 
         //to display the operation
@@ -56,12 +80,12 @@ function operate(x = "",y = "",op = ""){
 function OneOperation(x, y, op){
     this.num1 = x;
     this.num2 = y;
-    this.ans = operation(+x,+y,op)
+    this.ans = Math.round(operation(+x,+y,op) * 10000) / 10000
 }
 
 
 //to display the operations in display area
-function display(x,y,op,n = 0){
+function display(x,y,op,n = 0,x1=x){
     const topAns = document.querySelector(".topAns");
     const bottomAns = document.querySelector(".bottomAns")
 
@@ -70,14 +94,20 @@ function display(x,y,op,n = 0){
         topAns.textContent = `${allOperationArr[allOperationArr.length - 1].ans}`;
         bottomAns.textContent = "";
     }else if(n == 0){
-        topAns.textContent = `${x}${op}${y}`;
+        if(op!= "CLEAR"){
+            if(x == "" ){x = "0"}
+            topAns.textContent = `${x}${op}${y}`;
+        }
         //to display bottom ans(partial ans)
         if(y){
-            bottomAns.textContent = `${operation(+x,+y,op)}`
+            bottomAns.textContent = `${Math.round(operation(+x1,+y,op) * 10000) / 10000}`
         }
     }
 }
 
+
+//to refresh the calculator 
+document.querySelector(".clear").addEventListener("click",() => window.location.reload())
 //an array containing objects of operations
 allOperationArr = [];
 operate();
