7ed558663683a1e96ad874cdaf1c7b87add04d82
adhithiyan <adhithiyan.work@gmail.com>
2023-04-16 16:10:13 +0530

    working model for one single operation at a time

--- script.js
+++ script.js
@@ -15,29 +15,40 @@ function operation(x,y,op){
 function operate(x = "",y = "",op = ""){
     const btn = document.querySelectorAll("button")
     let num = "";
+    //to decide wheater top or bottom ans
+    let TopOrBottom;
     btn.forEach((n) => n.addEventListener("click",function(){
         if(this.textContent == "="){ 
             y = num;
+            
+            //push the operation into an array
             allOperationArr.push(new OneOperation(x,y,op));
-            console.log(allOperationArr[allOperationArr.length - 1].ans)
-            x  = y = op = num = "";
+            //to display the final ans
+            display(x,y,op,1)
+            TopOrBottom = 0;
+            //to continue the operation
+            num = allOperationArr[allOperationArr.length - 1].ans;
+            x = y = op = "";
         }else if(!Number.isInteger(+this.textContent)){  //checking if user clicked an operator
             x = num;
             op = this.textContent;
             num = "";
+            TopOrBottom = 1;
         }
         else{
             num += this.textContent;
+            TopOrBottom = 1;
         }
 
-        //to display partial answer
-        if(op && num){
-            console.log(operation(+x,+num,op))
+        //to display ans in display area
+        if(!op && TopOrBottom){
+            display(num,y,op)
+        }else if(op && TopOrBottom){
+            display(x,num,op)
         }
 
-        //to display the operatiosn
-        display(x,y,op);
-        console.log(num, x, y, op)        
+        //to display the operation
+        //console.log(num, x, y, op)        
     }))
 }
 
@@ -48,6 +59,24 @@ function OneOperation(x, y, op){
     this.ans = operation(+x,+y,op)
 }
 
+
+//to display the operations in display area
+function display(x,y,op,n = 0){
+    const topAns = document.querySelector(".topAns");
+    const bottomAns = document.querySelector(".bottomAns")
+
+    //to erase everything and display final ans
+    if(n != 0){
+        topAns.textContent = `${allOperationArr[allOperationArr.length - 1].ans}`;
+        bottomAns.textContent = "";
+    }else if(n == 0){
+        topAns.textContent = `${x}${op}${y}`;
+        //to display bottom ans(partial ans)
+        if(y){
+            bottomAns.textContent = `${operation(+x,+y,op)}`
+        }
+    }
+}
 
 //an array containing objects of operations
 allOperationArr = [];
